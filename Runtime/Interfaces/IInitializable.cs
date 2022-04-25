d546f7c3df2a8bc2118289c7d92217d709ac4e90
Cippman <cipponessuno@gmail.com>
2022-04-25 11:16:14 +0200

    added IInitializable interface

---
+++ Runtime/Interfaces/IInitializable.cs
@@ -0,0 +1,10 @@
+﻿namespace CippSharp.Core.Utils
+{
+    /// <summary>
+    /// Purpose: to class that have or need an initialization, when it is completed
+    /// </summary>
+    public interface IInitializable
+    {
+        bool IsInitialized { get; }
+    }
+}
